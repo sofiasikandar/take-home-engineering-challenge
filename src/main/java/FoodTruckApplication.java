16da3c279b53206f86232d41a1962d170dce7e59
sofiasikandar <35907509+sofiasikandar@users.noreply.github.com>
2022-01-25 16:27:02 -0500

    Update FoodTruckApplication.java

--- src/main/java/FoodTruckApplication.java
+++ src/main/java/FoodTruckApplication.java
@@ -1,2 +1,15 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class FoodTruckApplication {
+  private static final Logger logger = LoggerFactory.getLogger(FoodTruckApplication.class);
+
+  public static void main(String[] args) {
+    logger.info("Application Starting");
+    SpringApplication.run(FoodTruckApplication.class, args);
+    logger.info("Application Run Complete");
+  }
 }
